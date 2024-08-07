41b3e0367d40a4a540b4ab179f290b9300d426e3
fasol <fasol@DESKTOP-DEOR0M5>
2024-08-07 10:56:14 +0900

    refactor: 서블릿 http 세션으로 변경

--- board/src/main/java/egovframework/login/web/HomeController.java
+++ board/src/main/java/egovframework/login/web/HomeController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,10 +12,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import egovframework.example.sample.service.MainService;
 import egovframework.login.domain.member.Member;
 import egovframework.login.domain.member.MemberRepository;
+import egovframework.login.web.login.SessoinConst;
+import egovframework.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
@@ -22,13 +26,14 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class HomeController {
 	private final MemberRepository memberRepository;
+	private final SessionManager sessionManager;
 	
 //	@GetMapping("/")
 	public String home() {
 		return "home";
 	}
 	
-	@GetMapping("/")
+//	@GetMapping("/")
 	public String homeLogin(@CookieValue(name="memberId", required = false) Long memberId, Model model) {
 		if (memberId == null) {
 			return "home";
@@ -39,6 +44,56 @@ public class HomeController {
 			return "home";
 		}
 		
+		model.addAttribute("member", loginMember);
+		return "loginHome";
+	}
+	
+//	@GetMapping("/")
+	public String homeLoginV2(HttpServletRequest request, Model model) {
+		
+		// 세션 관리자에 저장된 회원 정보 조회
+		Member member = (Member)sessionManager.getSession(request);
+		
+		// 로그인
+		if (member == null) {
+			return "home";
+		}
+		
+		model.addAttribute("member", member);
+		return "loginHome";
+	}
+	
+//	@GetMapping("/")
+	public String homeLoginV3(HttpServletRequest request, Model model) {
+		
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			return "home";
+		}
+		
+		Member loginMember = (Member)session.getAttribute(SessoinConst.LOGIN_MEMBER);
+		
+		
+		// 세션에 회원 데이터가 없으면 home
+		if (loginMember == null) {
+			return "home";
+		}
+		
+		// 세션이 유지되면 로그인 이동
+		model.addAttribute("member", loginMember);
+		return "loginHome";
+	}
+	
+	@GetMapping("/")
+	public String homeLoginV3Spring(
+			@SessionAttribute(name= SessoinConst.LOGIN_MEMBER, required = false)Member loginMember, Model model) {
+		
+		// 세션에 회원 데이터가 없으면 home
+		if (loginMember == null) {
+			return "home";
+		}
+		
+		// 세션이 유지되면 로그인 이동
 		model.addAttribute("member", loginMember);
 		return "loginHome";
 	}
---
+++ board/src/main/java/egovframework/login/web/login/SessoinConst.java
@@ -0,0 +1,6 @@
+package egovframework.login.web.login;
+
+public class SessoinConst {
+	public static final String LOGIN_MEMBER = "loginMebmer";
+
+}
